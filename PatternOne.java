8cde544856b7d082948b540ce869bc1f98eda31f
Kartik pandey <69207248+kartik-99-ux@users.noreply.github.com>
2024-10-07 19:17:38 +0530

    Add files via upload

---
+++ PatternOne.java
@@ -0,0 +1,16 @@
+package dsa;
+
+public class PatternOne 
+{
+  public static void main(String arg[])
+  {
+	  for(int i=0;i<5;i++)
+	  {
+		  for(int j=0;j<5;j++)
+		  {
+			  System.out.print("*");
+		  }
+		  System.out.println("");
+	  }
+  }
+}
